373e29f41e1a2740ac72aa4153c50e2d3ba76963
IceBlasts <54529136+IceBlasts@users.noreply.github.com>
2022-03-16 15:19:40 +1100

    Created login and sign up

---
+++ app/src/main/java/com/example/smartbox_app/RegisterActivity.java
@@ -0,0 +1,77 @@
+package com.example.smartbox_app;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private EditText email, password;
+    private Button btnsignup;
+    private Button btnlogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+        email = findViewById(R.id.signup_email);
+        password = findViewById(R.id.signup_pass);
+        btnsignup = findViewById(R.id.btnsignup);
+        btnlogin = findViewById(R.id.btnlogin);
+
+        btnsignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Register();
+            }
+        });
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
+    }
+
+    private void Register() {
+        String user = email.getText().toString().trim();
+        String pass = password.getText().toString().trim();
+        if(user.isEmpty()){
+            email.setError("Email field can not be empty! ");
+        }
+        if(pass.isEmpty()){
+            password.setError("Password field can not be empty! ");
+        }
+        else{
+            mAuth.createUserWithEmailAndPassword(user,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        Toast.makeText(RegisterActivity.this, "Signed Up successfully. ", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+                    }
+                    else{
+                        Toast.makeText(RegisterActivity.this, "Sign up failed. Please try again. "+task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+            });
+        }
+    }
+
+}
